77b800c60ca4870f5e8b57a4d6314b68cd0b1265
Anna <Anna.polonnikova@list.ru>
2022-07-09 16:48:12 +0300

    commit1

---
+++ scripts/scripts.js
@@ -0,0 +1,18 @@
+//Задание 1 не поняла как вывести (Меня зовут...) и почему выводится всё в 1 строку?
+let FirstName = prompt('Ваше имя?');
+document.write( FirstName);
+
+ let age = prompt('Ваш возраст?');
+document.write( age );
+
+ let city = prompt('Ваш город проживания?');
+ document.write(city );
+
+ let phone = prompt('Контактный телефон');
+ document.write(phone );
+
+ let email = prompt('Ваш электронный адрес');
+ document.write(email );
+
+ let company = prompt('Ваше место работы');
+ document.write(company );
